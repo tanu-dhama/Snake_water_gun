e584c3f5dc58c7f1b28b7adbf16b2c11fa581f13
Tanu-dhama <tanudhama2508@gmail.com>
2025-06-17 20:17:26 +0530

    Add files via upload

---
+++ snake_water_gun.py
@@ -0,0 +1,34 @@
+# SNAKE WATER GUN
+import random
+
+'''
+1 for snake
+-1 for water 
+0 for gun 
+'''
+computer = random.choice([-1,0,1])
+youstr = input("enter your choice")
+youdict = {"s":1,"w":-1,"g":0}
+reversedict = {1:"snake",-1:"water",0:"gun"}
+
+you = youdict[youstr]
+
+print(f"you choose {reversedict[you]}\ncomputer choice {reversedict[computer]}")
+
+if (computer == you):
+    print("its a draw ")
+else:
+    if (computer == -1 and you == 1):
+        print("you win ")    
+    elif (computer == -1 and you == 0):
+        print("you loss ")    
+    elif (computer == 1 and you == -1):
+        print("you lose ")    
+    elif (computer == 1 and you == 0):
+        print("you win ")    
+    elif (computer == 0 and you == 1):
+        print("you lose ")    
+    elif (computer == 0 and you == -1):
+        print("you win ")    
+    else:
+         print("something went wrong")
